7a684ec28df43de0f472aacb0f12857b19d2bf1a
aymenby <aymenby30@gmail.com>
2021-03-10 01:46:07 +0100

    add .env

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
+require('dotenv').config({path:'./config/.env'})
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
-const port = 5000;
 
-// Read the host address and the port from the environment
-// const hostname =require(file.env.HOST) ;
-// const port = file.env.PORT;
-// const mongoURL = file.env.MONGO_URI;
+const user =  process.env.USER; 
+const password = process.env.PASSWORD;
+const Mongourl = `mongodb+srv://${user}:${password}@cluster0.zbckh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const port = process.env.PORT;
+const hostname = process.env.HOSTNAME;
 
-//connection DB to server
-const mongoURL = 'mongodb+srv://aymenby:aymenby123@cluster0.zbckh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 //Parse data
 app.use(express.json())
 app.use('/person', require('./Routes/personRoute'))
 
-mongoose.connect(mongoURL,{ useUnifiedTopology: true, useNewUrlParser: true },(err)=>{
+mongoose.connect(Mongourl,{ useUnifiedTopology: true, useNewUrlParser: true },(err)=>{
     err ? console.log(err) : console.log('DB is connected')
 })
 app.listen(port,(err)=>{
-    err ? console.log(err) : console.log(`Server running`)
+    err ? console.log(err) : console.log(`Server running on ${hostname}:${port}`)
 })
